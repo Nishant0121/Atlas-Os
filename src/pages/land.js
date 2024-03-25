36ccbdf1c8602a8b49ea12e9b860edd17c97beee
Nishant0121 <142099904+Nishant0121@users.noreply.github.com>
2024-03-25 21:31:49 +0530

    name

--- src/pages/land.js
+++ src/pages/land.js
@@ -16,14 +16,8 @@ export const Land = () => {
         <div className="mx-auto max-w-screen-xl px-4 py-32 flex justify-around  lg:flex lg:h-screen lg:items-center">
           <div className=" max-w-3xl flex items-center flex-col justify-between p-3 m-2  text-center ">
             <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
-              Understand User Flow.
-              <span className="sm:block"> Increase Conversion. </span>
+              Welcome to Atlas-OS
             </h1>
-
-            <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-              illo tenetur fuga ducimus numquam ea!
-            </p>
 
             <div className="mt-8 flex flex-wrap justify-center gap-4">
               <button
