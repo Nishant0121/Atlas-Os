465e060e26682e09ecce9af606e1c725693ffbba
Nishant0121 <142099904+Nishant0121@users.noreply.github.com>
2024-04-17 18:09:05 +0530

    json added

--- src/components/menubar.js
+++ src/components/menubar.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 import { ModeContext } from "../contex.js";
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,16 @@
 import React, { useContext } from "react";
-import youtube from "../logos/Youtube.png";
-import instagram from "../logos/Instagram.png";
-import google from "../logos/Google.png";
-import camera from "../logos/Camera.png";
-import expense from "../logos/expense.png";
-import weather from "../logos/weather.png";
-import calculator from "../logos/calculator.png";
-import fitness from "../logos/fitness.png";
-import dev from "../logos/dev.jpg";
 import { Link } from "react-router-dom";
 import StatusBar from "../components/statusbar.js";
 import TaskBar from "../components/taskbar.js";
+
+import apps from "../apps.json";
 
 import { ModeContext } from "../contex.js";
 
 export const Home = () => {
   const { isDarkMode } = useContext(ModeContext);
+
+  console.log(apps);
   return (
     <div>
       <div
@@ -24,59 +19,16 @@ export const Home = () => {
         }`}
       >
         <StatusBar />
+
         <div className="apps text-semibold">
-          <Link to={"/home/cam"} className="app">
-            <img src={camera} alt="" srcset="" />
-            <p>Camera</p>
-          </Link>
-          <div className="app">
-            <a href="https://www.youtube.com/">
-              <img src={youtube} alt="" srcset="" />
-            </a>
-            <p>YouTube</p>
-          </div>
-          <div className="app">
-            <a href="https://www.google.co.in/">
-              <img src={google} alt="" srcset="" />
-            </a>
-            <p>Google</p>
-          </div>
-          <div className="app">
-            <a href="https://www.instagram.com/">
-              <img src={instagram} alt="" srcset="" />
-            </a>
-            <p>Instagram</p>
-          </div>
-          <div className="app">
-            <a href="https://nishant0121.github.io/Calculator/">
-              <img src={calculator} alt="" srcset="" />
-            </a>
-            <p>Calculator</p>
-          </div>
-          <div className="app">
-            <a href="https://fitness-app-91739.web.app">
-              <img src={fitness} alt="" srcset="" />
-            </a>
-            <p>Bhago</p>
-          </div>
-          <div className="app">
-            <a href="https://buget-manager.web.app/">
-              <img src={expense} alt="" srcset="" />
-            </a>
-            <p>Expense</p>
-          </div>
-          <div className="app">
-            <a href="https://weatherapp-three-ashy.vercel.app/">
-              <img src={weather} alt="" srcset="" />
-            </a>
-            <p>Weather Info</p>
-          </div>
-          <div className="app">
-            <a href="https://portfolio-nishant.vercel.app/">
-              <img src={dev} alt="" srcset="" />
-            </a>
-            <p>About developer</p>
-          </div>
+          {apps.map((item, index) => (
+            <div className="app">
+              <Link to={`${item.link}`}>
+                <img src={item.img} alt="" srcset="" />
+                <p>{item.title}</p>
+              </Link>
+            </div>
+          ))}
         </div>
         <TaskBar />
       </div>
